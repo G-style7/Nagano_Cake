e97a4bb9aba5f49f46065e275d7393eb8df1f62b
EC2 Default User <ec2-user@ip-172-31-3-171.ap-northeast-1.compute.internal>
2022-09-10 14:02:45 +0000

    ~model

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,36 @@
 Rails.application.routes.draw do
+
+  namespace :admin do
+
+    root to: 'homes#top'
+    resources :items ,only:[:index, :new, :create, :show, :edit, :update]
+    resources :genres ,only:[:index, :create, :edit, :update]
+    resources :customers ,only:[:index, :show, :edit, :update] #顧客一覧など
+    resources :orders ,only:[:show, :update]
+    patch '/orders/:order_id/order_details/:id' => 'order_details#update' #製作ステータスの更新　　もしくはネストはなくてもよい /admin/order_details/:id
+
+
+  end
+
+
     scope module: :public do
+
     root to: 'homes#top'
     get '/about' => 'homes#about' ,as: 'about'
     resources :items ,only:[:index, :show]
-   
+    resources :customers ,only:[:show, :edit, :update]
+    get '/customers/unsubscribe' => 'customers#unsubscribe' #顧客の退会確認画面
+    get '/customers/withdraw' => 'customers#withdraw' #顧客の退会処理(ステータスの更新)
+    resources :cart_items ,only:[:index, :update, :create, :destroy]
+    delete '/cart_items/destroy_all' => 'cart_items#destroy_all'
+    resources :orders ,only:[:new, :index, :show, :create]
+    post '/orders/confirm' => 'orders#confirm' #注文情報確認画面
+    get '/orders/complete' => 'orders#complete' #注文完了画面
+    resources :addresses ,only:[:index, :edit, :create, :update, :destroy] #配送先情報など
 
   end
+
+
 # 顧客用
 # URL /customers/sign_in ...
   devise_for :customers,skip:[:passwords],controllers:{
