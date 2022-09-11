bb9493be7d1b449efbf09500a6a7bf6fafa137ee
EC2 Default User <ec2-user@ip-172-31-3-171.ap-northeast-1.compute.internal>
2022-09-11 14:42:58 +0000

    Log_in~

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -3,6 +3,12 @@
 class Admin::SessionsController < Devise::SessionsController
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  # def new
+  #   @admin = Admin.all
+  # end
+
+
+
   def after_sign_in_path_for(resource)
     admin_root_path
   end
@@ -10,6 +16,8 @@ class Admin::SessionsController < Devise::SessionsController
   def after_sign_out_path_for(resource)
     new_admin_session_path
   end
+
+
 
   # before_action :configure_sign_in_params, only: [:create]
 
