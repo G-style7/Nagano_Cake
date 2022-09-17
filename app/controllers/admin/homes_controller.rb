e82ce2f84f2ca4cd0e73fc93193f49d65aac9534
G-style7 <miyasato1989@gmail.com>
2022-09-17 06:38:13 +0000

    admin_sign_up_ok

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,6 +1,5 @@
 class Admin::HomesController < ApplicationController
   def top  #注文履歴一覧（adminのtop）
     @orders = Order.all
-    @order = Order_details.find(order.id)
   end
 end
--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -2,21 +2,26 @@ class Admin::ItemsController < ApplicationController
   def new
     @item = Item.new
   end
-  
+
   def create
     @item = Item.new(item_params)
     @item.save
     redirect_to admin_item_path(@item.id)
   end
-    
+
   def index
     @item =Item.all
-    
+
   end
-  
+
   def show
   end
 
   def edit
   end
+
+  private
+   def item_params
+    params.require(:item).permit(:name, :image, :introduction, :genre_id, :price, :is_active)
+   end
 end
