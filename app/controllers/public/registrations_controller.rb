538d66f40dd9bf92cf5cc5097955c15711317cd9
G-style7 <miyasato1989@gmail.com>
2022-09-18 06:28:17 +0000

    genre.item_ok

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -1,10 +1,17 @@
 # frozen_string_literal: true
 
 class Public::RegistrationsController < Devise::RegistrationsController
+  before_action :configure_permitted_parameters
   
   def after_sign_up_path_for(resource)
     customers_my_page_path #顧客のログイン後はマイページに遷移するため（顧客のshowページへ）
   end
+  
+  protected
+
+ def configure_permitted_parameters
+  devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code , :address , :telephone_number])
+ end
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
