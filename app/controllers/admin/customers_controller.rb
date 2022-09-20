f4baecf14eedcfa4b53f2c5d25d00ce94bfcbc1d
G-style7 <miyasato1989@gmail.com>
2022-09-20 06:07:52 +0000

    admin_customer_edit_ok

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -5,9 +5,25 @@ class Admin::CustomersController < ApplicationController
 
   def show
     @customer = Customer.find(params[:id])
+
   end
 
   def edit
     @customer = Customer.find(params[:id])
+  end
+
+  def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      flash[:notice] = "You have updated book successfully."
+    redirect_to admin_customer_path(@customer.id)
+    else
+    render :edit
+    end
+  end
+
+  private
+  def customer_params #privateメソッドの名前は、「モデル名_params」とすることが多い
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :email, :is_deleted) #bookはモデルのtitle.bodyカラムを承認
   end
 end
