29678f5582838c9d2b861c5fecd5f357d1afdb20
G-style7 <miyasato1989@gmail.com>
2022-09-21 14:16:43 +0000

    orders_new-menta

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,11 +1,38 @@
 class Public::OrdersController < ApplicationController
   def new
     @order = Order.new
+
+
   end
 
   def index
   end
 
   def show
+  end
+
+  def confirm
+   @order = Order.new(order_params) #注文情報に送るためだけのアクション？
+
+   if params[:select_address] == "1"
+     @order.postal_code = current_customer.postal_code #
+     @order.address = current_customer.address
+     @order.name = current_customer.first_name + current_customer.last_name
+   elsif params[:select_address] == "2"
+     @address = Address.find(params[:order][:address_id])
+     @order.postal_code = @address.postal_code
+     @order.address = @address.address
+     @order.name = @address.name
+   else
+     @order = Order.new(order_params) # params[:select_address] == "3"
+   end
+  end
+
+  #. ⬆️⬇️注文機能実装時に登録するデータでもあるためこの時点でストロングパラメーターに登録をし
+  #  注文(Order)モデルをnewメソッドで作成した際にデータが入るようにする
+private
+
+  def order_params
+  params.require(:order).permit(:payment_method, :postal_code, :address, :name) # ここにselect_addressはカラムにないため記述できない
   end
 end
--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -2,7 +2,7 @@
 
 class Public::SessionsController < Devise::SessionsController
   #before_action :configure_permitted_parameters, if: :devise_controller? 郵便番号等ログインに必要なデータの許可
-  
+
   def after_sign_in_path_for(resource) # topページへ遷移
     root_path
   end
@@ -10,10 +10,23 @@ class Public::SessionsController < Devise::SessionsController
   def after_sign_out_path_for(resource) # 顧客ログイン画面へ遷移
     new_customer_session_path
   end
-  
+
   #退会した場合はログインできないメソッドを作成
-  
-  
+
+   protected
+     # 退会しているかを判断するメソッド
+    def customer_state
+    ## 【処理内容1】 入力されたemailからアカウントを1件取得
+     @customer = Customer.find_by(email: params[:customer][:email])
+    ## アカウントを取得できなかった場合、このメソッドを終了する
+     return if !@customer
+    ## 【処理内容2】 取得したアカウントのパスワードと入力されたパスワードが一致してるかを判別
+     if @customer.valid_password?(params[:customer][:password])
+     ## 【処理内容3】
+     end
+    end
+     
+ end
 
   #before_action :configure_sign_in_params, only: [:create]
 
@@ -38,4 +51,4 @@ class Public::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
-end
+
