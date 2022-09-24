208bc3bf9412a2219b69a813d7bc39b5ae744077
G-style7 <miyasato1989@gmail.com>
2022-09-24 13:48:50 +0000

    cart_item_index

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,7 +1,10 @@
 class Public::CartItemsController < ApplicationController
   def index
-    @cart_items = CartItem.new
+    @cart_item = CartItem.new
     @items = Item.all
+    @cart_items= current_customer.cart_items.all
+    # カートに入ってる商品の合計金額
+    @total = @cart_items.inject(0) { |sum, item| sum + item.sum_price }
   end
 
   def create
@@ -9,16 +12,28 @@ class Public::CartItemsController < ApplicationController
     @cart_item.customer_id = current_customer.id
     @cart_item.save
     redirect_to cart_items_path
+  end
 
-
-
+  def update
+    @items = Item.find(params[:id])
+    if @items.update(cart_item_params)
+       redirect_to cart_items_path
+    else
+       render :index
+    end
   end
 
   def destroy
+    cart_item = Item.find(params[:id])
+    cart_item.destroy
+    redirect_to cart_items_path
 
   end
 
   def destroy_all
+    cart_items = CartItem.all
+    cart_items.destroy
+    redirect_to cart_items_path
 
   end
 
