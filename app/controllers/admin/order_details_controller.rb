b10c61dff4998c33a0c854e9212ed21ecce943f5
G-style7 <miyasato1989@gmail.com>
2022-10-04 10:05:45 +0000

    status

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -4,6 +4,7 @@ def update
     @order = Order.find(params[:order_id])
     @order_detail = OrderDetail.find(params[:id]) # @order.order_details.find(params[:id])と同じ
     if @order_detail.update(order_detail_params)
+      
       redirect_to admin_order_path(@order.id)
     else
       render admin_path(@order.id)
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -7,6 +7,11 @@ class Admin::OrdersController < ApplicationController
   def update
     @order = Order.find(params[:id])
     if @order.update(order_params)
+      if @order.status == "payment_confirmation" #1は入金確認
+         @order.order_details.each do |order_detail|
+           order_detail.update(making_status: 1)
+         end
+      end
       redirect_to admin_order_path(@order.id)
     else
       render :show
